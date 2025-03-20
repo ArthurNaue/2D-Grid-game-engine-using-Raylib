1dad155a04a68dd734a8bb03e481315f740848fe
Arthur Mazzardo Naue <arthur_naue@Hotmail.com>
2025-03-20 00:46:54 -0300

    code optimization

--- main.c
+++ main.c
@@ -3,8 +3,7 @@
 #include "include/raylib.h"
 
 //GAME SCREENS
-typedef enum eGameScreen { LOGO = 0, TITLE, GAMEPLAY, GAMEOVER} eGameScreen;
-
+typedef enum eGameScreen {LOGO = 0, TITLE, GAMEPLAY, GAMEOVER} eGameScreen;
 //GAME MAP
 int aMap[] ={
 	     3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
@@ -45,20 +44,17 @@ int aMap[] ={
              3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
              3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3
 	    };	
-
 //VECTOR TO MAP ARRAY INDEX FUNCTIONS 
-int check_true_x_position(int iAMapPosition) {
+int check_true_x_position(int iAMapPosition) 
+{
 	int iTruePosition = (iAMapPosition % 37) * 16;
-	
 	return iTruePosition;
 }
-int check_true_y_position(int iAMapPosition) {
+int check_true_y_position(int iAMapPosition) 
+{
 	int iTruePosition = (iAMapPosition / 37) * 16;
-
 	return iTruePosition;
 }
-
-
 //MAIN PROGRAM FUNCTION
 int main(void)
 {
@@ -68,13 +64,10 @@ int main(void)
     //WINDOW PARAMETERS
     const int iScreenWidth = 592;
     const int iScreenHeight = 592;
-
     //WINDOW INITIALIZATION
     InitWindow(iScreenWidth, iScreenHeight, "RAYLIB");
-
     //WINDOW CHOSEN
     eGameScreen eCurrentScreen = TITLE;
-
     //SET WINDOW FPS
     SetTargetFPS(60);                    
 
@@ -83,7 +76,6 @@ int main(void)
 //------------------------------------\\\GAME LOGIC\\\------------------------------------//
 
     int iPlayerPos = 0;
-
     //GAME LOOP FUNCTION
     while (!WindowShouldClose())    
     {
@@ -167,45 +159,42 @@ int main(void)
 
 //------------------------------------\\\DRAW\\\----------------------------------------//
 
+	void draw_background(Color eColor)
+	{
+		DrawRectangle(0, 0, iScreenHeight, iScreenWidth, eColor);
+	}
 	BeginDrawing();
-
             ClearBackground(RAYWHITE);
-
             switch(eCurrentScreen)
             {
                 case TITLE:
                 {
-		    DrawRectangle(0, 0, iScreenHeight, iScreenWidth, BLACK);
-                    DrawRectangle(200, 260, 200, 100, WHITE);
-                    DrawText("TESTE RAYLIB", 220, 300, 20, BLACK);
+		    draw_background(BLACK);
+                    DrawRectangle((iScreenHeight/2) - 100, (iScreenWidth/2) - 50, 200, 100, WHITE);
+                    DrawText("TESTE RAYLIB", (iScreenHeight/2) - 80, (iScreenWidth/2) - 10, 20, BLACK);
                 } break;
 
 //------------------------------------///MAIN GAME///------------------------------//
 
                 case GAMEPLAY:
        		{
-
 		    for(int i = 0; i < 1369; i++) {
-			    
 			switch(aMap[i])
 			    {
+				//DRAWING MAP INDEX SQUARE FUNCTION
+			    	void draw_square(Color eColor)
+				{
+					int aTrueSquarePosition[] = {check_true_x_position(i), check_true_y_position(i)};
+					DrawRectangle(aTrueSquarePosition[0], aTrueSquarePosition[1], 16, 16, eColor);	
+				}
 			    case 1:
 				{
+					//SET PLAYER POSITION TO MAP INDEX
 					iPlayerPos = i;
-					int aTrueSquarePosition[] = {check_true_x_position(i), check_true_y_position(i)};
-					DrawRectangle(aTrueSquarePosition[0], aTrueSquarePosition[1], 16, 16, GREEN);
-				} break;			
-			    case 2:
-			    	{
-		            		int aTrueSquarePosition[] = {check_true_x_position(i), check_true_y_position(i)}; 
-		    			DrawRectangle(aTrueSquarePosition[0], aTrueSquarePosition[1], 16, 16, RED);
-			    	} break;
-			    
-			    case 3:
-				{
-		            		int aTrueSquarePosition[] = {check_true_x_position(i), check_true_y_position(i)}; 
-					DrawRectangle(aTrueSquarePosition[0], aTrueSquarePosition[1], 16, 16, BLUE);
-				} break;
+					draw_square(GREEN);
+				}break;			
+			    case 2:{draw_square(RED);}break;
+			    case 3:{draw_square(BLUE);}break;
 			    default: break;
 			    }
 		    }
@@ -222,9 +211,9 @@ int main(void)
 	    	
 		case GAMEOVER:
 	    	{
-		    DrawRectangle(0, 0, iScreenHeight, iScreenWidth, BLACK);
-                    DrawRectangle(200, 260, 200, 100, WHITE);
-                    DrawText("GAME OVER", 220, 300, 20, BLACK);
+		    draw_background(BLACK);
+                    DrawRectangle((iScreenHeight/2) - 100, (iScreenWidth/2) - 50, 200, 100, WHITE);
+                    DrawText("GAME OVER", (iScreenHeight/2) - 80, (iScreenWidth/2) - 10, 20, BLACK);
 		} break;
                 default: break;
             }
