10897dfed8cd2999e5908f6a0bd213e84c5b2177
Arthur Mazzardo Naue <arthur_naue@Hotmail.com>
2025-03-20 01:51:26 -0300

    otimized code
    moved all the player movement logic inside a function

--- main.c
+++ main.c
@@ -1,58 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "include/raylib.h"
-
 //GAME SCREENS
 typedef enum eGameScreen {LOGO = 0, TITLE, GAMEPLAY, GAMEOVER} eGameScreen;
 //GAME MAP
 int aMap[] ={
-	     3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-             3, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3
+	     0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+             0, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+             0, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+             0, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+             3, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             3, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 	    };	
 //VECTOR TO MAP ARRAY INDEX FUNCTIONS 
 int check_true_x_position(int iAMapPosition) 
 {
-	int iTruePosition = (iAMapPosition % 37) * 16;
+	int iTruePosition = (iAMapPosition%37)*16;
 	return iTruePosition;
 }
 int check_true_y_position(int iAMapPosition) 
 {
-	int iTruePosition = (iAMapPosition / 37) * 16;
+	int iTruePosition = (iAMapPosition/37)*16;
 	return iTruePosition;
 }
 //MAIN PROGRAM FUNCTION
@@ -62,14 +61,14 @@ int main(void)
 //------------------------------------\\\WINDOW\\\----------------------------------------//
 
     //WINDOW PARAMETERS
-    const int iScreenWidth = 592;
-    const int iScreenHeight = 592;
+    const int iScreenWidth=592;
+    const int iScreenHeight=592;
     //WINDOW INITIALIZATION
     InitWindow(iScreenWidth, iScreenHeight, "RAYLIB");
     //WINDOW CHOSEN
-    eGameScreen eCurrentScreen = TITLE;
+    eGameScreen eCurrentScreen=TITLE;
     //SET WINDOW FPS
-    SetTargetFPS(60);                    
+    SetTargetFPS(60);
 
 //------------------------------------\\\WINDOW\\\----------------------------------------//
 
@@ -82,72 +81,51 @@ int main(void)
 	//VERIFY CURRENT SCREEN
         switch (eCurrentScreen)
         {
-            case TITLE:
-            {
-                if (IsKeyPressed(KEY_SPACE) || IsGestureDetected(GESTURE_TAP)){eCurrentScreen = GAMEPLAY;}
-            }break;
+            case TITLE:{if(IsKeyPressed(KEY_SPACE)||IsGestureDetected(GESTURE_TAP)){eCurrentScreen=GAMEPLAY;}}break;
             case GAMEPLAY:
             {
 
 //------------------------------------///PLAYER MOVEMENT///------------------------------//
+		
+		//PLAYER DIRECTION ENUM
+		typedef enum eDirection {UP = 0, DOWN, LEFT, RIGHT} eDirection;
 
-		if (IsKeyPressed(KEY_W))
+		//UPDATE MAP INDEX FOR PLAYER POSITION FUNCTION
+		void update_player_position(eDirection eDirection)
 		{
-			int iNewPlayerPos = iPlayerPos - 37;
-			int iNewPosition = aMap[iNewPlayerPos];
-			if(iNewPlayerPos < 0){iNewPlayerPos += 1369;}
-			switch(iNewPosition)
+			int iNewPlayerPos = iPlayerPos;
+			switch(eDirection)
 			{
-				case 0:
-					{
-						aMap[iPlayerPos] = 0;
-						aMap[iNewPlayerPos] = 1;
-					} break;
-				case 2:{eCurrentScreen = GAMEOVER;} break;
+				case UP:{iNewPlayerPos-=37;}break;
+				case DOWN:{iNewPlayerPos+=37;}break;
+				case LEFT:{iNewPlayerPos-=1;}break;
+				case RIGHT:{iNewPlayerPos+=1;}break;
+				default: break;	
 			}
-		}else if (IsKeyPressed(KEY_A))
-		{
-			int iNewPlayerPos = iPlayerPos - 1;
-			int iNewPosition = aMap[iNewPlayerPos];
-			if(iNewPlayerPos % 37 == 36 || iNewPlayerPos < 0){iNewPlayerPos += 37;}	
-			switch(iNewPosition)
+			//BOUNDARY CHECKS
+			switch(eDirection)
 			{
-				case 0:
-					{
-						aMap[iPlayerPos] = 0;
-						aMap[iNewPlayerPos] = 1;
-					} break;
-				case 2:{eCurrentScreen = GAMEOVER;} break;
+				case UP:{if(iNewPlayerPos<0){iNewPlayerPos+=1369;}}break;
+				case DOWN:{if(iNewPlayerPos>=1369){iNewPlayerPos-=1369;}}break;
+				case LEFT:{if(iNewPlayerPos%37==36||iNewPlayerPos<0){iNewPlayerPos+=37;}}break;
+				case RIGHT:{if(iNewPlayerPos%37==0){iNewPlayerPos-=37;}}break;
+				default: break;	
 			}
-		}else if (IsKeyPressed(KEY_S))
-		{
-			int iNewPlayerPos = iPlayerPos + 37;
 			int iNewPosition = aMap[iNewPlayerPos];
-			if(iNewPlayerPos >= 1369){iNewPlayerPos -= 1369;}
 			switch(iNewPosition)
 			{
 				case 0:
 					{
-						aMap[iPlayerPos] = 0;
-						aMap[iNewPlayerPos] = 1;
-					} break;
-				case 2:{eCurrentScreen = GAMEOVER;} break;
-			}
-		}else if (IsKeyPressed(KEY_D))
-		{
-			int iNewPlayerPos = iPlayerPos + 1;
-			int iNewPosition = aMap[iNewPlayerPos];
-			if(iNewPlayerPos % 37 == 0){iNewPlayerPos -= 37;}
-			switch(iNewPosition)
-			{
-				case 0:
-					{
-						aMap[iPlayerPos] = 0;
-						aMap[iNewPlayerPos] = 1;
+						aMap[iPlayerPos]=0;
+						aMap[iNewPlayerPos]=1;
 					} break;
 				case 2:{eCurrentScreen = GAMEOVER;} break;
 			}
 		}
+		if(IsKeyPressed(KEY_W)){update_player_position(UP);}
+		else if(IsKeyPressed(KEY_A)){update_player_position(LEFT);}
+		else if(IsKeyPressed(KEY_S)){update_player_position(DOWN);}
+		else if(IsKeyPressed(KEY_D)){update_player_position(RIGHT);}
 
 //------------------------------------///PLAYER MOVEMENT///------------------------------//
             
@@ -159,10 +137,8 @@ int main(void)
 
 //------------------------------------\\\DRAW\\\----------------------------------------//
 
-	void draw_background(Color eColor)
-	{
-		DrawRectangle(0, 0, iScreenHeight, iScreenWidth, eColor);
-	}
+	//DRAW BACKGROUND COLORED RECTANGLE FUNCTION
+	void draw_background(Color eColor){DrawRectangle(0, 0, iScreenHeight, iScreenWidth, eColor);}
 	BeginDrawing();
             ClearBackground(RAYWHITE);
             switch(eCurrentScreen)
@@ -170,15 +146,15 @@ int main(void)
                 case TITLE:
                 {
 		    draw_background(BLACK);
-                    DrawRectangle((iScreenHeight/2) - 100, (iScreenWidth/2) - 50, 200, 100, WHITE);
-                    DrawText("TESTE RAYLIB", (iScreenHeight/2) - 80, (iScreenWidth/2) - 10, 20, BLACK);
+                    DrawRectangle((iScreenHeight/2)-100, (iScreenWidth/2)-50, 200, 100, WHITE);
+                    DrawText("TESTE RAYLIB", (iScreenHeight/2)-80, (iScreenWidth/2)-10, 20, BLACK);
                 } break;
 
 //------------------------------------///MAIN GAME///------------------------------//
 
                 case GAMEPLAY:
        		{
-		    for(int i = 0; i < 1369; i++) {
+		    for(int i=0; i<1369; i++) {
 			switch(aMap[i])
 			    {
 				//DRAWING MAP INDEX SQUARE FUNCTION
@@ -190,7 +166,7 @@ int main(void)
 			    case 1:
 				{
 					//SET PLAYER POSITION TO MAP INDEX
-					iPlayerPos = i;
+					iPlayerPos=i;
 					draw_square(GREEN);
 				}break;			
 			    case 2:{draw_square(RED);}break;
@@ -199,11 +175,11 @@ int main(void)
 			    }
 		    }
 		    //DRAW MAP LINES 
-		    int iLines = 0;
-		    for (int i = 0; i < 37; i++) {
+		    int iLines=0;
+		    for (int i=0; i<37; i++) {
 			DrawLine(0, iLines, iScreenWidth, iLines, BLACK);
 		    	DrawLine(iLines, 0, iLines, iScreenHeight, BLACK);
-		        iLines = iLines + 16;	
+		        iLines += 16;	
 		    }
                 } break;
 
@@ -212,8 +188,8 @@ int main(void)
 		case GAMEOVER:
 	    	{
 		    draw_background(BLACK);
-                    DrawRectangle((iScreenHeight/2) - 100, (iScreenWidth/2) - 50, 200, 100, WHITE);
-                    DrawText("GAME OVER", (iScreenHeight/2) - 80, (iScreenWidth/2) - 10, 20, BLACK);
+                    DrawRectangle((iScreenHeight/2)-100, (iScreenWidth/2)-50, 200, 100, WHITE);
+                    DrawText("GAME OVER", (iScreenHeight/2)-80, (iScreenWidth/2)- 0, 20, BLACK);
 		} break;
                 default: break;
             }
@@ -221,8 +197,8 @@ int main(void)
     }
 
 //------------------------------------\\\DRAW\\\----------------------------------------//
-
+    
     //CLOSE THE GAME
-    CloseWindow();        
+    CloseWindow();
     return 0;
 }
