016f5fc68d590047cc1400b291288de0c89f4261
Arthur Mazzardo Naue <arthur_naue@Hotmail.com>
2025-03-23 11:08:24 -0300

    remade entire player movement to fit map resize

--- main.c
+++ main.c
@@ -2,64 +2,22 @@
 #include <stdlib.h>
 #include "include/raylib.h"
 
+#define iMapSize 24
+#define iGridSize 16
+
 //GAME SCREENS
 typedef enum eGameScreen {LOGO = 0, TITLE, GAMEPLAY, GAMEOVER} eGameScreen;
 
-//------------------------------------///MAP SIZE LOGIC///----------------------------------------//
-
-int iMapSize = 37;
-int iGridSize = 16;
-
-//------------------------------------///MAP SIZE LOGIC///----------------------------------------//
-
 //GAME MAP
-int aMap[] ={
-	     0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             0, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             0, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             0, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
-             3, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             3, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 4, 4, 0, 0, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-	    };	
-//VECTOR TO MAP ARRAY INDEX FUNCTIONS 
-int check_true_x_position(int iAMapPosition){int iTruePosition=(iAMapPosition%iMapSize)*iGridSize;return iTruePosition;}
-int check_true_y_position(int iAMapPosition){int iTruePosition=(iAMapPosition/iMapSize)*iGridSize;return iTruePosition;}
+int aMap[iMapSize][iMapSize] ={0};
+
+//VECTOR TO MAP ARRAY INDEX FUNCTION
+int check_true_position(int iAMapPosition){int iTruePosition=(iAMapPosition%iMapSize)*iGridSize;return iTruePosition;}
+
 //MAIN PROGRAM FUNCTION
 int main(void)
 {
-    //UPDATE MAP TRUE SIZE
-    int iMapTrueSize = iMapSize*iMapSize;
+    aMap[0][0] = 1;
 
 //------------------------------------\\\WINDOW\\\----------------------------------------//
 
@@ -76,7 +34,7 @@ int main(void)
 
 //------------------------------------\\\GAME LOGIC\\\------------------------------------//
 
-    int iPlayerPos = 0;
+    int iPlayerPos[] = {0, 0};
     //GAME LOOP FUNCTION
     while (!WindowShouldClose())    
     {
@@ -95,28 +53,33 @@ int main(void)
 		//UPDATE MAP INDEX FOR PLAYER POSITION FUNCTION
 		void update_player_position(eDirection eDirection)
 		{
-			int iNewPlayerPos = iPlayerPos;
+			int iNewPlayerPos[] = {iPlayerPos[0], iPlayerPos[1]};
 			switch(eDirection)
 			{
-				case UP:{iNewPlayerPos-=iMapSize;}break;
-				case DOWN:{iNewPlayerPos+=iMapSize;}break;
-				case LEFT:{iNewPlayerPos-=1;}break;
-				case RIGHT:{iNewPlayerPos+=1;}break;
+				case UP:{iNewPlayerPos[1]--;}break;
+				case DOWN:{iNewPlayerPos[1]++;}break;
+				case LEFT:{iNewPlayerPos[0]--;}break;
+				case RIGHT:{iNewPlayerPos[0]++;}break;
 				default:break;	
 			}
 			//BOUNDARY CHECKS
 			switch(eDirection)
 			{
-				case UP:{if(iNewPlayerPos<0){iNewPlayerPos+=iMapTrueSize;}}break;
-				case DOWN:{if(iNewPlayerPos>=iMapTrueSize){iNewPlayerPos-=iMapTrueSize;}}break;
-				case LEFT:{if(iNewPlayerPos%iMapSize==iMapSize-1||iNewPlayerPos<0){iNewPlayerPos+=iMapSize;}}break;
-				case RIGHT:{if(iNewPlayerPos%iMapSize==0){iNewPlayerPos-=iMapSize;}}break;
+				case UP:{if(iNewPlayerPos[1]<0){iNewPlayerPos[1]+=iMapSize;}}break;
+				case DOWN:{if(iNewPlayerPos[1]>=iMapSize){iNewPlayerPos[1]-=iMapSize;}}break;
+				case LEFT:{if(iNewPlayerPos[0]<0){iNewPlayerPos[0]+=iMapSize;}}break;
+				case RIGHT:{if(iNewPlayerPos[0]>=iMapSize){iNewPlayerPos[0]-=iMapSize;}}break;
 				default:break;	
 			}
-			int iNewPosition = aMap[iNewPlayerPos];
-			switch(iNewPosition)
+			int iNewMapPosition = aMap[iNewPlayerPos[1]][iNewPlayerPos[0]];
+			switch(iNewMapPosition)
 			{
-				case 0:{aMap[iPlayerPos]=iNewPosition; aMap[iNewPlayerPos]=1;}break;
+				case 0:
+					{
+					       aMap[iPlayerPos[1]][iPlayerPos[0]]=0;
+					       iPlayerPos[0]=iNewPlayerPos[0]; iPlayerPos[1]=iNewPlayerPos[1];
+					       aMap[iPlayerPos[1]][iPlayerPos[0]]=1;
+					}break;
 				case 2:{eCurrentScreen = GAMEOVER;}break;
 			}
 		}
@@ -152,31 +115,32 @@ int main(void)
 
                 case GAMEPLAY:
        		{
-		    for(int i=0; i<iMapTrueSize; i++) {
-			switch(aMap[i])
+		    //DRAWING MAP INDEX SQUARE FUNCTION
+	    	    void draw_square(Color eColor, int iPositionX, int iPositionY)	
+		    {
+			int aTrueSquarePosition[] = {check_true_position(iPositionX), check_true_position(iPositionY)};
+			DrawRectangle(aTrueSquarePosition[0], aTrueSquarePosition[1], iGridSize, iGridSize, eColor);	
+		    }
+		    for(int i=0; i<iMapSize; i++) 
+		    {
+			    for(int j=0; j<iMapSize; j++)
 			    {
-				//DRAWING MAP INDEX SQUARE FUNCTION
-			    	void draw_square(Color eColor)
-				{
-					int aTrueSquarePosition[] = {check_true_x_position(i), check_true_y_position(i)};
-					DrawRectangle(aTrueSquarePosition[0], aTrueSquarePosition[1], iGridSize, iGridSize, eColor);	
-				}
-			    case 1:
-				{
-					//SET PLAYER POSITION TO MAP INDEX
-					iPlayerPos=i;
-					draw_square(GREEN);
-				}break;			
-			    case 2:{draw_square(RED);}break;
-			    case 3:{draw_square(BLUE);}break;
-			    case 4:{draw_square(YELLOW);}break;
-			    case 5:{draw_square(BLACK);}break;
-			    default:break;
-			    }
+				switch(aMap[i][j])
+			    	{
+			    	case 1:
+					{
+						//SET PLAYER POSITION TO MAP INDEX
+						iPlayerPos[0]=j;
+						iPlayerPos[1]=i;
+						draw_square(GREEN, iPlayerPos[0], iPlayerPos[1]);
+					}break;			
+			    	default:break;
+			    	}
+			     }
 		    }
 		    //DRAW MAP LINES 
 		    int iLines=0;
-		    for (int i=0; i<=iMapSize-1; i++) 
+		    for (int i=0; i<=(iMapSize-1); i++) 
 		    {
 			DrawLine(0, iLines, iScreenWidth, iLines, BLACK);
 		    	DrawLine(iLines, 0, iLines, iScreenHeight, BLACK);
